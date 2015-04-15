3677d0bc4cc2238dc09a36a8475c235c8deb275d
Wal33D <aquataze@yahoo.com>
2015-04-15 01:28:02 -0400

    .

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -4,6 +4,7 @@ class TodoList < ActiveRecord::Base
 
   validates :title, presence: true
   validates :title, length: { minimum: 3 }
+  validates :title, uniqueness: true
   validates :description, presence: true
   validates :description, length: { minimum: 5 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Odot::Application.routes.draw do
   get "/login" => "user_sessions#new", as: :login
   delete "/logout" => "user_sessions#destroy", as: :logout
-
+  
   resources :users
   resources :user_sessions, only: [:new, :create]
   resources :password_resets, only: [:new, :create, :edit, :update]
